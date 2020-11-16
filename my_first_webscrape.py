6575c15b54c68ac02988e41f7f06358eb20e0182
John Kondwani Beni <johnbeni93@gmail.com>
2020-11-15 19:55:02 -0800

    Beta App runs once as proof of concept. First bug with retrieval of Size not fixed yet.

--- my_first_webscrape.py
+++ my_first_webscrape.py
@@ -3,7 +3,7 @@ from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup 
 
 
-my_url = 'https://www.amazon.com/s?k=iphone+8+cases&ref=nb_sb_noss_2'
+my_url = 'https://www.stillwhite.com/backless-wedding-dresses?size=4&size=6&price=0-1500&back=1&page=1&listing=199026'
 
 # opening connection, grabbing the page
 uClient = uReq(my_url)
@@ -13,4 +13,20 @@ uClient.close()
 # html parsing
 page_soup = soup(page_html, "html.parser")
 
-page_soup.h1
+containers = page_soup.findAll("li", {"data-page":"1"})
+
+for container in containers:
+    #
+    title_container = container.findAll("div",{"class":"item-title-heading"})
+    brand_name = title_container[0].text
+    #
+    price_container = container.findAll("div",{"class":"item-title-price"})
+    item_price = price_container[0].text
+
+    size_container = container.findAll("small",{"class":"text-muted"}) 
+    item_size = size_container[0].text.strip()
+
+
+print(brand_name)
+print(item_price)
+print(item_size)
