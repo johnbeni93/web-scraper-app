0474aa67490632f7e162da3d29a4ba2d3492b917
John Kondwani Beni <johnbeni93@gmail.com>
2020-11-15 22:44:20 -0800

    Beta App runs once as proof of concept. Retrieval of Size bug FIXED. Added parameters for savings percentage and item store link

--- my_first_webscrape.py
+++ my_first_webscrape.py
@@ -22,11 +22,22 @@ for container in containers:
     #
     price_container = container.findAll("div",{"class":"item-title-price"})
     item_price = price_container[0].text
-
+    #
     size_container = container.findAll("small",{"class":"text-muted"}) 
     item_size = size_container[0].text.strip()
+    for i in range(6):
+        size_value = item_size[i]
+        i += 1
+    #    
+    percentage_savings = item_size[-3:]
+    #
+    link_container = container.a.text
+    link_container_test = container.a ["href"]
+    #find('a'​)['href'])
 
 
 print(brand_name)
 print(item_price)
-print(item_size)
+print(size_value)
+print(percentage_savings)
+print("https://www.stillwhite.com/" + link_container_test)
