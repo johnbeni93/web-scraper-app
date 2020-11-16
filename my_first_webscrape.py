80d38e7728f3bb9ceff51301c7e6a05859854938
John Kondwani Beni <johnbeni93@gmail.com>
2020-11-15 23:39:36 -0800

    Beta runs compiling full .csv file. Added case handling incorrect savings percentage retireval. Scraping algorithm flexible enough to scrape any website. Try this version on your machine.

--- my_first_webscrape.py
+++ my_first_webscrape.py
@@ -2,7 +2,7 @@ import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup 
 
-
+# website url for scraping
 my_url = 'https://www.stillwhite.com/backless-wedding-dresses?size=4&size=6&price=0-1500&back=1&page=1&listing=199026'
 
 # opening connection, grabbing the page
@@ -13,8 +13,15 @@ uClient.close()
 # html parsing
 page_soup = soup(page_html, "html.parser")
 
+# grabs heach product
 containers = page_soup.findAll("li", {"data-page":"1"})
 
+# initiate .csv file open and write
+filename = "products.csv"
+f = open(filename, "w")
+headers = "brand, price, size, percent_savings, item_link\n"
+f.write(headers)
+ 
 for container in containers:
     #
     title_container = container.findAll("div",{"class":"item-title-heading"})
@@ -30,14 +37,23 @@ for container in containers:
         i += 1
     #    
     percentage_savings = item_size[-3:]
+    percent_comparison = "%"
+    if percentage_savings[-1] != "%":
+        percentage_savings = "N/A"
     #
     link_container = container.a.text
-    link_container_test = container.a ["href"]
-    #find('a'​)['href'])
+    link_container_test = container.a["href"]
+    #
+    f.write(brand_name + "," + item_price.replace(",","'") + "," + size_value + "," + percentage_savings + "," + "https://www.stillwhite.com/" + link_container_test + "\n")
+    
+f.close()
+print("Check products.csv folder in direcotry for latest scrape!")
 
-
-print(brand_name)
-print(item_price)
-print(size_value)
-print(percentage_savings)
+# pronto testing kit
+"""
+print("brand: " + brand_name)
+print("price: " +item_price)
+print("size: " + size_value)
+print("percent. savings: " + percentage_savings)
 print("https://www.stillwhite.com/" + link_container_test)
+"""
